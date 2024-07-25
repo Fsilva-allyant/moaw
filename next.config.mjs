0edc420b6b8e20d33a2c4095284aa739f19e13f7
Fsilva-allyant <fsilva@allyant.com>
2024-07-25 16:45:51 -0300

    changed nextjs configuration to export standalone entrypoint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone",
+  distDir: ".next",
+};
 
 export default nextConfig;
