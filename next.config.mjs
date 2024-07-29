8d1eca375417aca171847f53d5025c60a4e87a1a
Fsilva-allyant <fsilva@allyant.com>
2024-07-29 12:23:24 -0300

    updated next config to add static routes and to disable image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: { unoptimized: true },
   output: "export",
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      "/": { page: "/" },
+      "/accessible": { page: "/accessible" },
+      "/inaccessible": { page: "/inaccessible" },
+    };
+  },
   //   distDir: ".next",
 };
 
