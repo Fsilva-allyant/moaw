d772d27dbe98d75bda086e0eda764ce07c80af6b
Fsilva-allyant <fsilva@allyant.com>
2024-08-01 13:57:37 -0300

    HeroBannerNA > removed heading, created unnecesary list, link issues: href and accessible names

--- components/Hero/HeroBannerNA.jsx
+++ components/Hero/HeroBannerNA.jsx
@@ -14,19 +14,21 @@ export default function HeroBannerNA() {
     >
       <VStack w="full" justify="center" px={["1rem", "2rem"]} bgColor="blue.base" opacity="0.9">
         <Stack maxW="50%" align="flex-start" spacing="2rem" textAlign="center">
-          <Heading as="h1" size={["h2", "h1"]} variant="white">
+          <Heading as="span" size={["h2", "h1"]} variant="white">
             Let&apos;s improve the usability of our empathy
           </Heading>
-          <Text variant="white" size={["sm", "md"]}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua
-          </Text>
+          <ul>
+            <Text as="li" variant="white" size={["sm", "md"]}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua
+            </Text>
+          </ul>
           <Button
             as="a"
-            href="#"
             variant="white"
             size={["md", "lg"]}
             _focus={{ outline: "3px solid white", outlineOffset: "2px" }}
+            aria-label="Contact Us"
           >
             Read more
           </Button>
