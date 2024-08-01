b5934cfe95029d45e1708aee51272e17696a09c7
Fsilva-allyant <fsilva@allyant.com>
2024-08-01 15:29:11 -0300

    updated feature cards icon component styling

--- components/Features/FeatureCardsNA.jsx
+++ components/Features/FeatureCardsNA.jsx
@@ -5,18 +5,18 @@ import { HiLightBulb } from "react-icons/hi";
 
 const Feature = ({ title, text, icon }) => {
   return (
-    <Stack alignItems={["center", "start"]} textAlign={["center", "start"]}>
+    <Stack alignItems={["center", "start"]} textAlign={["center", "start"]} maxW="90%">
       <Flex
         color="white"
         rounded="full"
         bg="gray.light"
-        w="4rem"
-        h="4rem"
+        w="3.5rem"
+        h="3.5rem"
         mb="0.25rem"
         align="center"
         justify="center"
       >
-        {icon}
+        <Icon maxWidth="2.5rem" aria-hidden="true" as={icon} color="blue.base" w="4rem" h="4rem" />
       </Flex>
       <Heading as="h3" size="h3" variant="blue">
         {title}
@@ -36,17 +36,17 @@ export default function FeatureCards() {
       </Heading>
       <SimpleGrid columns={[1, 3]} spacing="2.5rem">
         <Feature
-          icon={<Icon aria-hidden="false" as={HiLightBulb} color="blue.base" w="4rem" h="4rem" />}
+          icon={HiLightBulb}
           title="Rissus Commodo"
           text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore..."
         />
         <Feature
-          icon={<Icon aria-hidden="false" as={HiLightBulb} color="blue.base" w="4rem" h="4rem" />}
+          icon={HiLightBulb}
           title="Rissus Commodo"
           text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore..."
         />
         <Feature
-          icon={<Icon aria-hidden="false" as={HiLightBulb} color="blue.base" w="4rem" h="4rem" />}
+          icon={HiLightBulb}
           title="Rissus Commodo"
           text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore..."
         />
